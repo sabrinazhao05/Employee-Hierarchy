83c6261aba0713c5bcede888500886ffe4e901c3
Sabrina Zhao <71298505+sabrinazhao05@users.noreply.github.com>
2025-03-10 15:14:08 -0400

    Update Employee.cpp

--- Employee.cpp
+++ Employee.cpp
@@ -1,7 +1,3 @@
-//DISPLAY 15.2 Implementation for the Base Class Employee
-//This is the file: employee.cpp. 
-//This is the implementation for the class Employee.
-//The interface for the class Employee is in the header file employee.h.
 #include <string>
 #include <cstdlib>
 #include <iostream>
@@ -12,48 +8,38 @@ using namespace std;
 
 Employee::Employee( ) : name("No name yet"), ssn("No number yet")
 {
-    //deliberately empty
-    //cout <<"Employee() called\n";
 }
 
 Employee::Employee(string the_name, string the_number) 
        : name(the_name), ssn(the_number)
 {
-    //deliberately empty
-    //cout <<"Employee( " <<the_name <<","<<the_number <<")called\n";
 }
 
-// get_name() - returns name
 string Employee::get_name( ) const 
 {
     return name;
 }
 
-// get_ssn() - returns ssn
 string Employee::get_ssn( ) const 
 {
     return ssn;
 }
 
-// set_name(new_name) - sets the name to new_name
 void Employee::set_name(string new_name)
 {
     name = new_name;
 }
 
-// set_ssn(new_ssn) - sets the ssn to new_ssn
 void Employee::set_ssn(string new_ssn)
 {
     ssn = new_ssn;
 }
 
-// get_net_pay() - calculate and return the net_pay
 double Employee::get_net_pay() const
 {
 	return 0.0;
 }
 
-// print_check() - prints a portion of the paycheck
 void Employee::print_check( ) const
 {
     cout << "\n_______________________________________________\n" ;
@@ -70,14 +56,12 @@ istream & Employee::promptInput(istream & in, ostream & out)
 	return operator>>(in, *this);
 }
 
-// utility is protected
 istream& Employee::ignorespace(istream& in) const
 {
 	if (!isspace(in.get())) in.unget();
 	return in;
 }
 
-// input operator
 istream& operator>>(istream& in, Employee& empl)
 {
 	empl.ignorespace(in);
