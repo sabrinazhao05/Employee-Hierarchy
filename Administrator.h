125bfb10bdf1f8ea1b3d336fef2a8c6e252ae66b
Sabrina Zhao <71298505+sabrinazhao05@users.noreply.github.com>
2025-03-10 15:12:49 -0400

    Update Administrator.h

--- Administrator.h
+++ Administrator.h
@@ -1,10 +1,3 @@
-/* This is the header file for Administrator class */
-/* Please finish the definition of the class to provide the following additional members:
- *  1. a string type member variable to store the title (director, Vice President, ... )
- *  3. Constructor functions: one default one, another taking necessary info. about the administrator: name, SSN, salary, title
- *  4. redefine print_check() so that the title is printed on the check. Start with the version from 
- *  SalariedEmployee
- */
 #ifndef ADMINISTRATOR_H
 #define ADMINISTRATOR_H
 
